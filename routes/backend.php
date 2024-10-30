11c5ff277e726a9ce39a0c72f7d6ef28c9181167
MD-IQBAL-HOSAIN <iqbalmd5692@gmail.com>
2024-10-30 08:58:38 +0600

    remove test

--- routes/backend.php
+++ routes/backend.php
@@ -24,7 +24,7 @@ Route::controller(SystemSettingController::class)->group(function () {
     Route::get('/system/stripe', 'stripeindex')->name('stripe.index');
     Route::post('/system/stripe', 'stripestore')->name('stripe.store');
     
-aaaaaaaaaaaaaaaaaaaaaaaaa
+
     // Route::get('/system/paypal', 'paypalindex')->name('paypal.index');
     // Route::post('/system/paypal', 'paypalstore')->name('paypal.store');
     //  Route::post('/pro', 'paypalstore')->name('paypal.store');
